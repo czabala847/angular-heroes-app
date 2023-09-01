f225fa29039a99a02bdb6799e0e24315b915837d
Carlos Zabala <czabala847@gmail.com>
2023-09-01 17:28:30 -0500

    reactive form

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,13 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
   styles: [],
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit {
+  public heroForm = new FormGroup({
+    id: new FormControl(''),
+    superhero: new FormControl('', { nonNullable: true }),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  });
+
   public publishers = [
     { id: 'DC Comics', desc: 'DC - Comics' },
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
   ];
+
+  constructor(
+    private heroService: HeroesService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    if (!this.router.url.includes('edit')) {
+      return;
+    }
+
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => {
+          return this.heroService.getHeroById(id);
+        })
+      )
+      .subscribe((hero) => {
+        if (!hero) {
+          return this.router.navigateByUrl('/');
+        }
+
+        this.heroForm.reset(hero);
+
+        return;
+      });
+  }
+
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+
+  onSubmit(): void {
+    if (!this.heroForm.valid) return;
+
+    if (this.currentHero.id) {
+      this.heroService.editHero(this.currentHero).subscribe((hero) => {
+        console.log(hero);
+        return;
+      });
+    }
+
+    this.heroService.addHero(this.currentHero).subscribe((hero) => {
+      console.log(hero);
+    });
+  }
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { Observable, map, of, switchMap } from 'rxjs';
 
-import { BDResponse, Hero } from '../interfaces/hero.interface';
+import { BDResponse, Hero, Publisher } from '../interfaces/hero.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -36,6 +36,40 @@ export class HeroesService {
             hero.superhero.toLowerCase().includes(query.toLowerCase())
           )
           .slice(0, 5);
+      })
+    );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    const idPublisher = hero.publisher === Publisher.DCComics ? 'dc' : 'marvel';
+    const newHero: Hero = {
+      ...hero,
+      id: `${hero.superhero.toLowerCase()}-${idPublisher}-${new Date().getTime()}`,
+    };
+
+    return this.getData().pipe(
+      map((data) => {
+        const newHeroes = [...data.heroes, newHero];
+        return { ...data, heroes: newHeroes };
+      }),
+      switchMap((updateData) => {
+        this.http.put(this.jsonUrl, updateData);
+
+        return of(newHero);
+      })
+    );
+  }
+
+  editHero(hero: Hero): Observable<Hero> {
+    return this.getData().pipe(
+      map((data) => {
+        const newHeroes = data.heroes.map((h) => (h.id === hero.id ? hero : h));
+        return { ...data, heroes: newHeroes };
+      }),
+      switchMap((updateData) => {
+        this.http.put(this.jsonUrl, updateData);
+
+        return of(hero);
       })
     );
   }
